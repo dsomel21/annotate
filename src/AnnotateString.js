3fd711ced60cb4ebdeee87c6bfd1e4bf7e41440c
Dilraj <dsomel21@gmail.com>
2021-09-03 22:28:43 -0400

    Cleaned up code

--- src/AnnotateString.js
+++ src/AnnotateString.js
@@ -1,64 +1,92 @@
 import React, { useState } from 'react';
 
-const AnnotateString = (props) => {
+const AnnotateString = props => {
   if (props) console.log('props.selectionType', props.selectionType);
 
   const [selectionType, setSelectionType] = useState(props.selectionType || 'FULL_STRING');
-  //  /\r|\n/
-  const containsLineBreak = (text) => /\r|\n/.exec(text) !== null;
+
+  const containsLineBreak = text => /\r|\n/.exec(text) !== null;
 
   const NON_VALUE_CHARS = [' ', ',', '.', ';', '?'];
-  const startsEmpty = (text) => NON_VALUE_CHARS.includes(text[0]);
-  const endsEmpty = (text) => NON_VALUE_CHARS.includes(text[text.length - 1]);
-  const charIsEmpty = (char) => NON_VALUE_CHARS.includes(char);
+  const startsEmpty = text => NON_VALUE_CHARS.includes(text[0]);
+  const endsEmpty = text => NON_VALUE_CHARS.includes(text[text.length - 1]);
+  const charIsEmpty = char => NON_VALUE_CHARS.includes(char);
 
-  const onMouseUpHandler = (e) => {
-    const selectionObj = window.getSelection && window.getSelection();
-    if (!selectionObj.toString()) return;
+  const onMouseUpHandler = e => {
+    const selObj = window.getSelection && window.getSelection();
+    if (!selObj.toString()) return;
 
-    return selectify(selectionObj);
+    const finalSelObj = selectify(selObj);
+    return {
+      getSelection: finalSelObj,
+      selectedText: finalSelObj.toString(),
+      range: [finalSelObj.anchorOffset, finalSelObj.focusOffset],
+      wholeText: finalSelObj.anchorNode.wholeText,
+      target: e.target,
+    };
+  };
+
+  /* NOTE: Selection.modify is not fully supported */
+  const highlightFullLine = selObj => {
+    const { anchorNode } = selObj;
+    selObj.extend(anchorNode, anchorNode.textContent.length);
+    // Selects everything to the LEFT of the beginning of the paragraph
+    selObj.modify('extend', 'left', 'paragraphboundary');
+  };
+
+  /* 
+    NOTE: The purpose of this method is to carefully SELECT the full words
+    form the window.getSelection(). If will trim away any NON_VALUE_CHARS
+    and may increase the characters on the LEFT or RIGHT if necessary to 
+    complete the word. 
+
+    NOTE: There is sometimes weird behaviour if the sentence has periods.
+    For some reason, the anchorNode does not include ALL the text
+  */
+  const highlightFullWords = selObj => {
+    const { anchorNode, anchorOffset, focusOffset } = selObj;
+    const { wholeText } = anchorNode;
+    const selectionText = selObj.toString();
+    let leftOffset = anchorOffset;
+    let rightOffset = focusOffset - 1;
+
+    if (startsEmpty(selectionText)) {
+      // Move the leftOffset forward, because there may be preceding whitespace, punctuation, etc.
+      while (charIsEmpty(wholeText[leftOffset])) leftOffset++;
+    } else if (leftOffset > 0) {
+      // Move the leftOffset back, because there may be letters towards the left of it
+      while (!charIsEmpty(wholeText[leftOffset - 1]) && leftOffset > 0) leftOffset--;
+    }
+
+    if (endsEmpty(selectionText)) {
+      // Move the rightOffset back, because there may be TRAILING whitespace, punctuation, etc.
+      while (charIsEmpty(wholeText[rightOffset])) rightOffset--;
+    } else if (rightOffset < wholeText.length) {
+      // Move the rightOffset forward, because there may be letters to the right of it
+      while (!charIsEmpty(wholeText[rightOffset + 1]) && rightOffset < wholeText.length)
+        rightOffset++;
+    }
+
+    // Change the Selection on the Browser
+    const range = document.createRange();
+    range.setStart(anchorNode, leftOffset);
+    range.setEnd(anchorNode, Math.min(rightOffset + 1, wholeText.length));
+    selObj.removeAllRanges();
+    selObj.addRange(range);
   };
 
   // Modifies the existing window.Selection
-  const selectify = (selectionObj) => {
-    const { anchorNode, anchorOffset, focusOffset } = selectionObj;
-    const { wholeText } = anchorNode;
-    const selectionText = selectionObj.toString();
+  const selectify = selObj => {
     switch (selectionType) {
       case 'FULL_STRING':
-        selectionObj.extend(anchorNode, anchorNode.textContent.length);
-        // Selects everything to the LEFT of the beginning of the paragraph
-        selectionObj.modify('extend', 'left', 'paragraphboundary');
+        highlightFullLine(selObj);
         break;
       case 'FULL_WORDS': // No trailing, preceding whitespace, punctuation
-        let leftOffset = anchorOffset;
-        let rightOffset = focusOffset - 1;
-        if (startsEmpty(selectionText)) {
-          // Move the leftOffset forward, because there may be preceding whitespace, punctuation, etc.
-          while (charIsEmpty(wholeText[leftOffset])) leftOffset++;
-        } else if (leftOffset > 0) {
-          // Move the leftOffset back, because there may be letters towards the left of it
-          while (!charIsEmpty(wholeText[leftOffset - 1]) && leftOffset > 0) leftOffset--;
-        }
-
-        if (endsEmpty(selectionText)) {
-          // Move the rightOffset back, because there may be TRAILING whitespace, punctuation, etc.
-          while (charIsEmpty(wholeText[rightOffset])) rightOffset--;
-        } else if (rightOffset < wholeText.length) {
-          while (!charIsEmpty(wholeText[rightOffset + 1]) && rightOffset < wholeText.length)
-            rightOffset++;
-        }
-        const w = wholeText.slice(leftOffset, rightOffset + 1);
-        console.log(w);
-        let range = document.createRange();
-        range.setStart(anchorNode, leftOffset);
-        range.setEnd(anchorNode, Math.min(rightOffset + 1, wholeText.length));
-        selectionObj.removeAllRanges();
-        selectionObj.addRange(range);
+        highlightFullWords(selObj);
         break;
       default:
-        selectionObj.modify('extend', 'left', 'paragraphboundary');
-        selectionObj.modify('extend', 'right', 'paragraphboundary');
+        selObj.modify('extend', 'left', 'paragraphboundary');
+        selObj.modify('extend', 'right', 'paragraphboundary');
         break;
     }
     return window.getSelection();
--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Annotate from './Annotate';
 function App() {
-  const randomSpace = `.`;
   return (
     <div className='App'>
       <header className='App-header'>
@@ -12,40 +11,25 @@ function App() {
         </p>
         <a
           className='App-link'
-          href='https://reactjs.org'
+          href='https://spg.dev'
           target='_blank'
           rel='noopener noreferrer'
         >
           Learn React
         </a>
         <br />
-        <br />
 
         <Annotate selectionType='FULL_WORDS'>
-          <p key={1222}>DIlraj has a cool fast mustang shelby</p>
-
-          <p key={10}>
-            ਸ਼੍ਰੀ ਸਤਿਗੁਰੁ ਨਦ ਤੇ ਉਤਰਿ,,, {randomSpace} ਆਇ ਦਮਦਮੇ ਥਾਨ? !
-          </p>
+          <p key={10}>ਸ਼੍ਰੀ ਸਤਿਗੁਰੁ ਨਦ ਤੇ ਉਤਰਿ,,,,, ਆਇ ਦਮਦਮੇ ਥਾਨ? !</p>
           <p key={11}>ਧਰਮਸਾਲ ਸੁੰਦਰ ਰਚੀ, ਮਿਲੇ ਸਿੱਖ੍ਯ ਗਨ ਆਨ</p>
         </Annotate>
+
         <Annotate selectionType='FULL_STRING'>
           <p key={12}>ਜਹਿਂ ਧੋਬਨਿ ਕੋ ਬ੍ਰਿੱਛ ਟਿਕਾਯਹੁ</p>
           <p key={13}>ਏਕ ਗ੍ਰਾਮ ਤੌ ਤਹਾਂ ਬਸਾਯਹੁ</p>
           <p key={14}>ਅਪਰ ਥਾਨ ਖੰਜਰ ਜੋ ਲਹ੍ਯੋ</p>
+          <p key={15}>ਅਵਨੀ ਬਿਖੈ ਗਾਡ ਸੋ ਦਯੋ</p>
         </Annotate>
-
-        <p>ਅਵਨੀ ਬਿਖੈ ਗਾਡ ਸੋ ਦਯ</p>
-        <p>ਤਹਿਂ ਇਕ ਗ੍ਰਾਮ ਬਸਾਯੋ ਭਾਰੀ</p>
-        <p>ਖੰਜਰ ਤਾਂ ਕੋ ਨਾਮ ਉਚਾਰੀ</p>
-        <p>ਪਾਤਿਸ਼ਾਹਿ ਕੀ ਕੇਤਿਕ ਸੈਨਾ</p>
-        <p>ਤਹਾਂ ਟਿਕਾਵਨਿ ਕੀਨਿ ਸੁਖੈਨ</p>
-        <p>ਹੱਦ ਕਰੀ ਦੁਇ ਰਾਜਨਿ ਕੇਰੀ</p>
-        <p>ਜਿਸ ਤੇ ਉਠਹਿ ਬਿਰੋਧ ਨ ਫੇਰੀ</p>
-        <p>ਦੁਹਿ ਦਿਸ਼ਿ ਕੋ ਕਰਿ ਹਰਖ ਸਮੇਤ</p>
-        <p>ਆਪ ਆਪਨੇ ਰਾਜ ਸੁਚੇ</p>
-        <p>ਦੇਸ਼ ਕਾਮਰੂ ਕੇਰ ਬਰੋਬਰ</p>
-        <p>ਅਪਰ ਰਾਜ ਇਕ ਤਹਾਂ ਹੁਤੋ ਬਰ</p>
       </header>
     </div>
   );
