6297d2c8001ce9d053be7ff5b72dfa478d855d29
Dilraj <dsomel21@gmail.com>
2021-09-06 14:46:26 -0400

    Added the forgotten  action

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Annotate from './Annotate';
 function App() {
   const handleSelect = selection => {
-    debugger;
+    console.log(selection);
   };
 
   return (
@@ -24,11 +24,13 @@ function App() {
         <br />
 
         <Annotate selectionType='FULL_WORDS' onSelect={handleSelect}>
+          <p>This, is the Full Words</p>
           <p key={10}>ਸ਼੍ਰੀ ਸਤਿਗੁਰੁ ਨਦ ਤੇ ਉਤਰਿ,,,,, ਆਇ ਦਮਦਮੇ ਥਾਨ? !</p>
           <p key={11}>ਧਰਮਸਾਲ ਸੁੰਦਰ ਰਚੀ, ਮਿਲੇ ਸਿੱਖ੍ਯ ਗਨ ਆਨ</p>
         </Annotate>
 
-        <Annotate selectionType='FULL_STRING'>
+        <Annotate selectionType='FULL_STRING' onSelect={handleSelect}>
+          <p>This is the FULL_STRING, and should grab the entire string no matter what....</p>
           <p key={12}>ਜਹਿਂ ਧੋਬਨਿ ਕੋ ਬ੍ਰਿੱਛ ਟਿਕਾਯਹੁ</p>
           <p key={13}>ਏਕ ਗ੍ਰਾਮ ਤੌ ਤਹਾਂ ਬਸਾਯਹੁ</p>
           <p key={14}>ਅਪਰ ਥਾਨ ਖੰਜਰ ਜੋ ਲਹ੍ਯੋ</p>
