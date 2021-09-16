71b9b5beea083b4c7fa7a3699c2d339e4eb39252
Dilraj <dsomel21@gmail.com>
2021-09-16 10:19:52 -0400

    Applied formatting

--- tests/full_string.test11.js
+++ tests/full_string.test11.js
@@ -1,103 +1,100 @@
 //Unit Testing with Selenium and Jest
 /**
  * @jest-environment jest-environment-webdriver
- * 
+ *
  * Visit jest documentation at: https://jestjs.io/docs/api
  */
 
- const {By, Builder, until} = require("selenium-webdriver");
- require("chromedriver");
-  
- const url = 'http://localhost:3000/'   // This url needs to be changed to the website
- var page = '';
- const test_username = 'user';
- const test_password = 'pass';
+const { By, Builder, until } = require('selenium-webdriver');
+require('chromedriver');
 
- // We use this to make sure the page is loaded befor trying to get the elements
- const documentInitialised = () =>
-    driver.executeScript('return document.readyState');
+const url = 'http://localhost:3000/'; // This url needs to be changed to the website
+var page = '';
+const test_username = 'user';
+const test_password = 'pass';
 
- jest.setTimeout(30000); // Jest normally times out due to some asynchronous leakages. This is used to delay the timeouts
- var driver;
+// We use this to make sure the page is loaded befor trying to get the elements
+const documentInitialised = () => driver.executeScript('return document.readyState');
+
+jest.setTimeout(30000); // Jest normally times out due to some asynchronous leakages. This is used to delay the timeouts
+var driver;
 
 // This runs before all the tests
- beforeAll( async () => {
-     // Here is where we load the browser. 
-    // You can change it to firefox. Other browsers are supported although slight issues may occur
-    driver = await new Builder().forBrowser("chrome").build(); 
-    // await driver.manage().window().setRect({ width: 1024, height: 768 });
-    await driver.manage().window().setRect({ width: 1600, height: 768 });
-    // await new Promise((r) => setTimeout(r, 10000));
+beforeAll(async () => {
+  // Here is where we load the browser.
+  // You can change it to firefox. Other browsers are supported although slight issues may occur
+  driver = await new Builder().forBrowser('chrome').build();
+  // await driver.manage().window().setRect({ width: 1024, height: 768 });
+  await driver.manage().window().setRect({ width: 1600, height: 768 });
+  // await new Promise((r) => setTimeout(r, 10000));
 });
 
 // This runs after all the tests are run
 afterAll(async () => {
-    // await driver.quit(); // Closes the browser
-  }, 15000);
+  // await driver.quit(); // Closes the browser
+}, 15000);
 
 // This is used to group related tests together.
 // Only one describe is used here but as the project becomes bigger, it may be necessary to ...
 // group them into different describe blocks
 describe('This should highlight full line.', () => {
-    // This is a single test
-    it('Should open the annotate url', async () => {
-        // const res = await driver.get('https://developer.mozilla.org');
-        const res = await driver.get(url);
-        await new Promise((r) => setTimeout(r, 2000));
-        page = 'MainPage';
-        // await driver.executeScript("monitorEvents($0, 'mouse')");
-    } )
+  // This is a single test
+  it('Should open the annotate url', async () => {
+    // const res = await driver.get('https://developer.mozilla.org');
+    const res = await driver.get(url);
+    await new Promise(r => setTimeout(r, 2000));
+    page = 'MainPage';
+    // await driver.executeScript("monitorEvents($0, 'mouse')");
+  });
 
-    describe("This should test FULL_STRING", () => {
-        let myArr;
-        let full_string;
-        let data;
-        beforeAll(async () => {
-            const line_elements = await driver.findElements(By.css('.FULL_STRING p'));
-            const line_element  = line_elements[0];
-            const line_element1  = line_elements[1];
+  describe('This should test FULL_STRING', () => {
+    let myArr;
+    let full_string;
+    let data;
+    beforeAll(async () => {
+      const line_elements = await driver.findElements(By.css('.FULL_STRING p'));
+      const line_element = line_elements[0];
+      const line_element1 = line_elements[1];
 
-            console.log(line_element);
-            full_string = await line_element.getText();
+      console.log(line_element);
+      full_string = await line_element.getText();
 
-            console.log("full_string: ", full_string);
+      console.log('full_string: ', full_string);
 
-            let sourceEle = line_element;
-            let targetEle = line_element1;
+      let sourceEle = line_element;
+      let targetEle = line_element1;
 
-            await driver.executeScript("arguments[0].scrollIntoView(true);", line_element);
+      await driver.executeScript('arguments[0].scrollIntoView(true);', line_element);
 
-            let offset = await targetEle.getRect();
-            let x = await offset.x;
-            let y = await offset.y;
-            const actions = driver.actions({async: true});
+      let offset = await targetEle.getRect();
+      let x = await offset.x;
+      let y = await offset.y;
+      const actions = driver.actions({ async: true });
 
-            await actions.move({origin:sourceEle}).press().perform();
-            await actions.move({origin:targetEle}).release().pause(3000).perform();
+      await actions.move({ origin: sourceEle }).press().perform();
+      await actions.move({ origin: targetEle }).release().pause(3000).perform();
 
-            await driver.executeScript("console.log(4837473);");
-            data = await driver.executeScript("return window.getSelection().toString();");
-            myArr = data.split(" ");
-        })
-        it('Should confirm that only one line was grabbed', async () => {
-            
-            expect(myArr.length).toBeGreaterThan(0);
-
-            console.log(myArr);
-            
-            for(let e of myArr) {
-                // Notice that if the next line is grabbed, this will fail. "full_string" is first line and it does not contain words in the second line
-                expect(full_string).toContain(e); 
-            }
-        } )
-
-        it('Should confirm that the full line was grabbed', async () => {
-            
-            expect(myArr.length).toBeGreaterThan(0);
-
-            console.log(myArr);
-            
-            expect(data).toEqual(full_string);
-        } )
+      await driver.executeScript('console.log(4837473);');
+      data = await driver.executeScript('return window.getSelection().toString();');
+      myArr = data.split(' ');
     });
-})
+    it('Should confirm that only one line was grabbed', async () => {
+      expect(myArr.length).toBeGreaterThan(0);
+
+      console.log(myArr);
+
+      for (let e of myArr) {
+        // Notice that if the next line is grabbed, this will fail. "full_string" is first line and it does not contain words in the second line
+        expect(full_string).toContain(e);
+      }
+    });
+
+    it('Should confirm that the full line was grabbed', async () => {
+      expect(myArr.length).toBeGreaterThan(0);
+
+      console.log(myArr);
+
+      expect(data).toEqual(full_string);
+    });
+  });
+});
--- tests/game.js
+++ tests/game.js
@@ -1,240 +1,233 @@
 //Unit Testing with Selenium and Jest
 /**
  * @jest-environment jest-environment-webdriver
- * 
+ *
  * Visit jest documentation at: https://jestjs.io/docs/api
  */
 
- const {By, Builder, until} = require("selenium-webdriver");
- require("chromedriver");
-  
- const url = 'http://localhost/3000'   // This url needs to be changed to the website
- var page = '';
- const test_username = 'user';
- const test_password = 'pass';
+const { By, Builder, until } = require('selenium-webdriver');
+require('chromedriver');
 
- // We use this to make sure the page is loaded befor trying to get the elements
- const documentInitialised = () =>
-    driver.executeScript('return document.readyState');
+const url = 'http://localhost/3000'; // This url needs to be changed to the website
+var page = '';
+const test_username = 'user';
+const test_password = 'pass';
 
- jest.setTimeout(30000); // Jest normally times out due to some asynchronous leakages. This is used to delay the timeouts
- var driver;
+// We use this to make sure the page is loaded befor trying to get the elements
+const documentInitialised = () => driver.executeScript('return document.readyState');
+
+jest.setTimeout(30000); // Jest normally times out due to some asynchronous leakages. This is used to delay the timeouts
+var driver;
 
 // This runs before all the tests
- beforeAll( async () => {
-     // Here is where we load the browser. 
-    // You can change it to firefox. Other browsers are supported although slight issues may occur
-    driver = await new Builder().forBrowser("chrome").build(); 
-    // await new Promise((r) => setTimeout(r, 10000));
+beforeAll(async () => {
+  // Here is where we load the browser.
+  // You can change it to firefox. Other browsers are supported although slight issues may occur
+  driver = await new Builder().forBrowser('chrome').build();
+  // await new Promise((r) => setTimeout(r, 10000));
 });
 
 // This runs after all the tests are run
 afterAll(async () => {
-    await driver.quit(); // Closes the browser
-  }, 15000);
+  await driver.quit(); // Closes the browser
+}, 15000);
 
 // This is used to group related tests together.
 // Only one describe is used here but as the project becomes bigger, it may be necessary to ...
 // group them into different describe blocks
 describe('This should run basic tests on game.', () => {
-    
-    // This is a single test
-    it('Should open the game url', async () => {
-        // const res = await driver.get('https://developer.mozilla.org');
-        const res = await driver.get(url);
-        await new Promise((r) => setTimeout(r, 2000));
-        page = 'MainPage';
-    } )
+  // This is a single test
+  it('Should open the game url', async () => {
+    // const res = await driver.get('https://developer.mozilla.org');
+    const res = await driver.get(url);
+    await new Promise(r => setTimeout(r, 2000));
+    page = 'MainPage';
+  });
 
-    describe('it captures a screenshot of the current page on the browser', () => {
-        it('snap a picture by taking the screenshot', async () => {
-        //   await browser.takeScreenshot()
-          await driver.takeScreenshot().then(
-            function(image, err) {
-                require('fs').writeFile(page + '.png', image, 'base64', function(err) {
-                    console.log(err);
-                });
-            }
-        );
-        })
-      })
+  describe('it captures a screenshot of the current page on the browser', () => {
+    it('snap a picture by taking the screenshot', async () => {
+      //   await browser.takeScreenshot()
+      await driver.takeScreenshot().then(function (image, err) {
+        require('fs').writeFile(page + '.png', image, 'base64', function (err) {
+          console.log(err);
+        });
+      });
+    });
+  });
 
-    it('Should successfully go to the login page', async () => {
-        const button = await driver.findElement(By.css('.loginbtn'));
-        await button.click();
-        page = 'LoginPage'
-    } )
+  it('Should successfully go to the login page', async () => {
+    const button = await driver.findElement(By.css('.loginbtn'));
+    await button.click();
+    page = 'LoginPage';
+  });
 
-    describe('it captures a screenshot of the current page on the browser', () => {
-        it('snap a picture by taking the screenshot', async () => {
-        //   await browser.takeScreenshot()
-          await driver.takeScreenshot().then(
-            function(image, err) {
-                require('fs').writeFile(page + '.png', image, 'base64', function(err) {
-                    console.log(err);
-                });
-            }
-        );
-        })
-      })
+  describe('it captures a screenshot of the current page on the browser', () => {
+    it('snap a picture by taking the screenshot', async () => {
+      //   await browser.takeScreenshot()
+      await driver.takeScreenshot().then(function (image, err) {
+        require('fs').writeFile(page + '.png', image, 'base64', function (err) {
+          console.log(err);
+        });
+      });
+    });
+  });
 
-      it('Should successfully login', async () => {driver.executeScript('return document.readyState');
-        const username_input = await driver.findElement(By.name('username'));
-        const password_input = await driver.findElement(By.name('password'));
+  it('Should successfully login', async () => {
+    driver.executeScript('return document.readyState');
+    const username_input = await driver.findElement(By.name('username'));
+    const password_input = await driver.findElement(By.name('password'));
 
-        await driver.executeScript("arguments[0].setAttribute('value', '" + test_username +"')", username_input);
-        await driver.executeScript("arguments[0].setAttribute('value', '" + test_password +"')", password_input);
+    await driver.executeScript(
+      "arguments[0].setAttribute('value', '" + test_username + "')",
+      username_input
+    );
+    await driver.executeScript(
+      "arguments[0].setAttribute('value', '" + test_password + "')",
+      password_input
+    );
 
-        const button = await driver.findElement(By.name('loginuser'));
-        await button.click();
-        page = 'HomePage'
-    } )
+    const button = await driver.findElement(By.name('loginuser'));
+    await button.click();
+    page = 'HomePage';
+  });
 
-    describe('it captures a screenshot of the current page on the browser', () => {
-        it('snap a picture by taking the screenshot', async () => {
-        //   await browser.takeScreenshot()
-          await driver.takeScreenshot().then(
-                function(image, err) {
-                    require('fs').writeFile(page + '.png', image, 'base64', function(err) {
-                        console.log(err);
-                    });
-                }
-            );
-        })
-      })
+  describe('it captures a screenshot of the current page on the browser', () => {
+    it('snap a picture by taking the screenshot', async () => {
+      //   await browser.takeScreenshot()
+      await driver.takeScreenshot().then(function (image, err) {
+        require('fs').writeFile(page + '.png', image, 'base64', function (err) {
+          console.log(err);
+        });
+      });
+    });
+  });
 
-    it('Should check for navigations', async () => {
-        var failed = false;
+  it('Should check for navigations', async () => {
+    var failed = false;
 
-        const buttons = await driver.findElements(By.css('button'));
-        const gameNavs = ['Maths', 'English', 'Verbal Reasoning', 'Non Verbal Reasoning', 'Study Time']
-        const resultTexts = []
-        for(let e of buttons) {
-            resultTexts.push(await e.getText());
+    const buttons = await driver.findElements(By.css('button'));
+    const gameNavs = [
+      'Maths',
+      'English',
+      'Verbal Reasoning',
+      'Non Verbal Reasoning',
+      'Study Time',
+    ];
+    const resultTexts = [];
+    for (let e of buttons) {
+      resultTexts.push(await e.getText());
+    }
+
+    for (let e of gameNavs) {
+      // console.log(e);
+      expect(resultTexts).toContain(e); // Checks if the texts of the buttons are in the gameNavs defined above
+    }
+  });
+
+  it('Should successfully click on the addition link.', async () => {
+    const actions = driver.actions(); // Mouse actions can be performed with this
+
+    const buttons = await driver.findElements(By.css('button')); // We use the normal css selectors to find the html elements.
+
+    var found = false; // This is used to avoid continuously searching after an element has been found and clicked
+
+    // console.log(dropdowns);
+    // console.log(await getElementXpath(driver, "//html[1]/body[1]/div[1]") );
+    count = 0;
+    for (let e of buttons) {
+      // We don't want to waste time in the for loop if we still have found what we're looking for
+      if (found) {
+        break;
+      }
+
+      // We simulate moving the mouse on top of the button Only after we hover over the button can we see the dropdown menu
+      await actions.move({ duration: 1000, origin: e }).perform();
+      var dropdown = await driver.findElement(By.css('.dropdown-menu'));
+
+      // We use the dropdown to get the <a> elements under it and not all the <a> existing in the page. !! Important
+      let aElem = await dropdown.findElements(By.css('a'));
+
+      for (let ae of aElem) {
+        var a_text = await ae.getText(); // Getting text
+
+        // e is a button. we check if it is the Maths button.
+        // a_text is the <a> link. We check if it is the addition
+        if ((await e.getText()) == 'Maths' && a_text == 'Addition') {
+          await ae.click(); // And then we click
+          found = true;
+          break;
         }
+      }
+    }
 
-        for(let e of gameNavs) {
-            // console.log(e);
-            expect(resultTexts).toContain(e); // Checks if the texts of the buttons are in the gameNavs defined above
-        }
-    } )
+    const play_button = await driver.findElement(By.linkText('Play')); // Getting an element using its inner text
 
-    it('Should successfully click on the addition link.', async () => {
-        const actions = driver.actions(); // Mouse actions can be performed with this
+    expect(await play_button.getText()).toEqual('Play');
+  });
 
-        const buttons = await driver.findElements(By.css('button')); // We use the normal css selectors to find the html elements.
+  it('Should successfully click on play.', async () => {
+    const actions = driver.actions({ bridge: true });
+    const play_button = await driver.findElement(By.linkText('Play'));
+    await play_button.click();
 
-        var found = false; // This is used to avoid continuously searching after an element has been found and clicked
+    page = 'GamePage';
+  });
 
-        // console.log(dropdowns);
-        // console.log(await getElementXpath(driver, "//html[1]/body[1]/div[1]") );
-        count = 0;
-        for(let e of buttons) {
+  describe('it captures a screenshot of the current page on the browser', () => {
+    it('snap a picture by taking the screenshot', async () => {
+      //   await browser.takeScreenshot()
+      await driver.takeScreenshot().then(function (image, err) {
+        require('fs').writeFile(page + '.png', image, 'base64', function (err) {
+          console.log(err);
+        });
+      });
+    });
+  });
 
-            // We don't want to waste time in the for loop if we still have found what we're looking for
-            if(found){
-                break; 
-            }
+  it('Game page should have 4 choices.', async () => {
+    // let ele = await driver.wait(until.elementLocated(By.css('choice-container')),10000);
+    // const choice_container = await driver.findElement(By.css('.choice-container'));
+    const choice_prefixes = await driver.findElements(By.css('.choice-prefix'));
+    const choice_texts = await driver.findElements(By.css('.choice-text'));
 
-            // We simulate moving the mouse on top of the button Only after we hover over the button can we see the dropdown menu
-            await actions.move({duration:1000,origin:e}).perform();
-            var dropdown = await driver.findElement(By.css('.dropdown-menu'));
+    // console.log(choice_prefixes);
+    expect(choice_prefixes.length).toEqual(4);
+    expect(choice_texts.length).toEqual(4);
+  });
 
-            // We use the dropdown to get the <a> elements under it and not all the <a> existing in the page. !! Important
-            let aElem = await dropdown.findElements(By.css('a'));
+  it('Should randomly answer all questions.', async () => {
+    // let ele = await driver.wait(until.elementLocated(By.css('choice-container')),10000);
+    // const choice_container = await driver.findElement(By.css('.choice-container'));
+    const choices = ['A', 'B', 'C', 'D'];
+    var choice_prefixes = await driver.findElements(By.css('.choice-prefix'));
 
-            for(let ae of aElem){
-                var a_text = await ae.getText(); // Getting text
+    while (choice_prefixes.length > 0) {
+      var ans_index = Math.floor(Math.random() * (4 - 1) + 1); // We randomly select an answer
 
-                // e is a button. we check if it is the Maths button. 
-                // a_text is the <a> link. We check if it is the addition
-                if(await e.getText() == 'Maths' && a_text == 'Addition'){ 
-                    await ae.click(); // And then we click
-                    found = true;
-                    break;
-                }
-            }
-        }
+      await choice_prefixes[ans_index].click();
+      // We use this to slow down and wait for the questions to refresh. You can reduce or increase the value as you see fit.
+      await new Promise(r => setTimeout(r, 1000));
 
-        const play_button = await driver.findElement(By.linkText('Play')); // Getting an element using its inner text
+      // choice_prefixes = await driver.wait(until.elementLocated(By.css('choice-prefix')),5000);
+      choice_prefixes = await driver.findElements(By.css('.choice-prefix'));
+    }
 
-        expect(await play_button.getText()).toEqual("Play");
-    } )
+    await driver.wait(() => documentInitialised(), 10000);
+    const end_elements = await driver.findElements(By.id('end'));
+    expect(end_elements.length).toEqual(1); // We are expecting an element with end id
+    expect(await end_elements[0].getText()).toContain('Play Again'); // We are expecting "Play Again" in the end id
 
-    it('Should successfully click on play.', async () => {
-        const actions = driver.actions({bridge: true});
-        const play_button = await driver.findElement(By.linkText('Play'));
-        await play_button.click();
+    page = 'EndPage';
+  });
 
-        page = 'GamePage';
-
-    } )
-
-    describe('it captures a screenshot of the current page on the browser', () => {
-        it('snap a picture by taking the screenshot', async () => {
-        //   await browser.takeScreenshot()
-          await driver.takeScreenshot().then(
-                function(image, err) {
-                    require('fs').writeFile(page + '.png', image, 'base64', function(err) {
-                        console.log(err);
-                    });
-                }
-            );
-        })
-      })
-
-    it('Game page should have 4 choices.', async () => {
-        // let ele = await driver.wait(until.elementLocated(By.css('choice-container')),10000);
-        // const choice_container = await driver.findElement(By.css('.choice-container'));
-        const choice_prefixes = await driver.findElements(By.css('.choice-prefix'));
-        const choice_texts = await driver.findElements(By.css('.choice-text'));
-        
-        // console.log(choice_prefixes);
-        expect(choice_prefixes.length).toEqual(4);
-        expect(choice_texts.length).toEqual(4);
-
-    } )
-
-    it('Should randomly answer all questions.', async () => {
-        // let ele = await driver.wait(until.elementLocated(By.css('choice-container')),10000);
-        // const choice_container = await driver.findElement(By.css('.choice-container'));
-        const choices = ['A', 'B', 'C', 'D'];
-        var choice_prefixes = await driver.findElements(By.css('.choice-prefix'));
-
-        while(choice_prefixes.length > 0){
-            var ans_index = Math.floor(Math.random() * (4 - 1) + 1); // We randomly select an answer
-
-            await choice_prefixes[ans_index].click();
-            // We use this to slow down and wait for the questions to refresh. You can reduce or increase the value as you see fit.
-            await new Promise((r) => setTimeout(r, 1000)); 
-
-            // choice_prefixes = await driver.wait(until.elementLocated(By.css('choice-prefix')),5000);
-            choice_prefixes = await driver.findElements(By.css('.choice-prefix'));
-        }
-        
-        await driver.wait(() => documentInitialised(), 10000);
-        const end_elements = await driver.findElements(By.id('end'));
-        expect(end_elements.length).toEqual(1); // We are expecting an element with end id
-        expect(await end_elements[0].getText()).toContain('Play Again'); // We are expecting "Play Again" in the end id
-
-        page = "EndPage";
-
-    } )
-
-    describe('it captures a screenshot of the current page on the browser', () => {
-        it('snap a picture by taking the screenshot', async () => {
-        //   await browser.takeScreenshot()
-          await driver.takeScreenshot().then(
-                function(image, err) {
-                    require('fs').writeFile(page + '.png', image, 'base64', function(err) {
-                        console.log(err);
-                    });
-                }
-            );
-        })
-      })
-
-
-
-
-})
+  describe('it captures a screenshot of the current page on the browser', () => {
+    it('snap a picture by taking the screenshot', async () => {
+      //   await browser.takeScreenshot()
+      await driver.takeScreenshot().then(function (image, err) {
+        require('fs').writeFile(page + '.png', image, 'base64', function (err) {
+          console.log(err);
+        });
+      });
+    });
+  });
+});
